3e39f7bba6921c2b21ddf462da2c73fac9b06ac8
Aleksander Wędrychowski <aleksander.wedrychowski@alior.pl>
2021-01-04 16:39:13 +0100

    Initial commit (project setup)

---
+++ Landmark-SwiftUI/Landmark_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_SwiftUIApp.swift
+//  Landmark-SwiftUI
+//
+//  Created by Aleksander Wędrychowski on 04/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
